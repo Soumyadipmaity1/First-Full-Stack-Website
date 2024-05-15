8e70631d43a01ff4a56f844c834f61ee3d3a6a99
smilingsoumya1 <soumyadipmaityjee1@gmail.com>
2024-05-15 23:19:49 +0530

    Add axios dependency and fetch jokes from backend API

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import axios from 'axios'
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [jokes, setJokes] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3000/jokes').then(response => {
+      setJokes(response.data)
+    })
+  },) 
   return (
     <>
-     <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <p>JOKES:{jokes.length}</p>
+      {
+        jokes.map(joke => (
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
     </>
   )
 }
 
-export default App
+export default App;
